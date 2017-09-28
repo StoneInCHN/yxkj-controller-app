f9bd2c5d18ecadeac3143252312b1a215d54b22b
zengqiang <zengqiang@yxsh-tech.com>
2017-09-28 16:22:17 +0800

    ui修改

---
+++ app/src/main/java/com/yxkj/controller/broadcast/BootBroadcastReceiver.java
@@ -0,0 +1,27 @@
+package com.yxkj.controller.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.yxkj.controller.activity.MainActivity;
+
+/**
+ *
+ */
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    /**
+     * demo2: 可以实现开机自动打开软件并运行。
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //MainActivity就是开机显示的界面
+        Intent mBootIntent = new Intent(context, MainActivity.class);
+        //下面这句话必须加上才能开机自动运行app的界面
+        mBootIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(mBootIntent);
+    }
+}
