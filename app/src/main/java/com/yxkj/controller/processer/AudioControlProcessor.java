b13e3d7d70a18517b650e08f5f32fb40e7cd8320
huyong <82370288@qq.com>
2017-10-17 10:37:15 +0800

    1.更新cmdMsg到0.0.4

--- app/src/main/java/com/yxkj/controller/processer/AudioControlProcessor.java
+++ app/src/main/java/com/yxkj/controller/processer/AudioControlProcessor.java
@@ -1,12 +1,9 @@
 package com.yxkj.controller.processer;
 
-import com.easivend.evprotocol.EVprotocol;
-import com.yxkj.controller.application.MyApplication;
-import com.yxkj.controller.beans.EV_json;
 import com.yxkj.controller.tools.ControlAudioManager;
-import com.yxkj.controller.util.GsonUtil;
-import com.yxkj.controller.util.LogUtil;
 import com.yxkj.entity.CmdMsg;
+
+import java.util.Map;
 
 import io.netty.channel.ChannelHandlerContext;
 
@@ -20,7 +17,8 @@ public class AudioControlProcessor implements IProcessor {
 
     @Override
     public void process(ChannelHandlerContext ctx, CmdMsg cmdMsg) {
-        ControlAudioManager.newInstance().setVolume(Float.parseFloat(cmdMsg.getContent()));
+        Map<String, String> contentMap = cmdMsg.getContent();
+        ControlAudioManager.newInstance().setVolume(Float.parseFloat(contentMap.getOrDefault("volume", "10")));
     }
 
     @Override
